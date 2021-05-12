ff2d2e37f00c6a38e513a308524bd568cdb5c5b9
Luis G. Villegas <luisguve@gmail.com>
2021-05-12 14:20:36 -0300

    Introduced feature to wait for players if they disconnect. Store Pgn in moves. Inform players when the opponent disconnects, reconnects and leaves the room.

--- player.go
+++ player.go
@@ -46,14 +46,11 @@ type player struct {
 	// The websocket connection.
 	conn *websocket.Conn
 
-	// Buffered channel of outbound moves.
-	sendMove chan []byte
-
-	// Buffered channel of outbound messages.
-	sendChat chan message
-
-	// Channel to know when the opponent's clock reached zero.
-	oppRanOut chan bool
+	// Events channels
+	sendMove   chan []byte
+	sendChat   chan message
+	oppRanOut  chan bool
+	disconnect chan bool
 
 	// Action channels
 	drawOffer          chan bool
@@ -61,19 +58,25 @@ type player struct {
 	oppResigned        chan bool
 	rematchOffer       chan bool
 	oppAcceptedRematch chan bool
+	oppReady           chan bool
+	oppDisconnected    chan bool
+	oppGone            chan bool
+	oppReconnected     chan bool
 
-	cleanup  func()
-	color    string
-	gameId   string
-	timeLeft time.Duration
-	clock    *time.Timer
-	lastMove time.Time
-	username string
-	userId   string
+	cleanup      func()
+	switchColors func()
+	color        string
+	gameId       string
+	timeLeft     time.Duration
+	clock        *time.Timer
+	lastMove     time.Time
+	username     string
+	userId       string
 }
 
 type move struct {
 	Color string `json:"color"`
+	Pgn   string `json:"pgn"`
 	move  []byte
 }
 
@@ -88,6 +91,7 @@ type message struct {
 	GameOver      bool   `json:"gameOver"`
 	RematchOffer  bool   `json:"rematchOffer"`
 	AcceptRematch bool   `json:"acceptRematch"`
+	FinishRoom    bool   `json:"finishRoom"`
 	userId        string
 }
 
@@ -98,7 +102,8 @@ type message struct {
 // reads from this goroutine.
 func (p *player) readPump() {
 	defer func() {
-		p.room.unregister <-p
+		p.room.disconnect<- p
+		p.sendMove = nil
 		p.conn.Close()
 	}()
 	p.conn.SetReadLimit(maxMessageSize)
@@ -107,8 +112,12 @@ func (p *player) readPump() {
 	for {
 		_, msg, err := p.conn.ReadMessage()
 		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				log.Printf("error: %v", err)
+			if websocket.IsUnexpectedCloseError(err,
+				websocket.CloseGoingAway,
+				websocket.CloseAbnormalClosure,
+				websocket.CloseNormalClosure,
+			) {
+				log.Printf("%v player connection is gone with error: %v", p.color, err)
 			}
 			break
 		}
@@ -143,6 +152,8 @@ func (p *player) readPump() {
 			p.room.broadcastRematchOffer<- p.color
 		case m.AcceptRematch:
 			p.room.broadcastAcceptRematch<- p.color
+		case m.FinishRoom:
+			return
 		default:
 			log.Println("Unexpected message", m)
 		}
@@ -162,6 +173,9 @@ func (p *player) writePump() {
 	}()
 	for {
 		select {
+		case <-p.disconnect:
+			// Finish this goroutine to not to send messages anymore
+			return
 		case move, ok := <-p.sendMove: // Opponent moved a piece
 			p.conn.SetWriteDeadline(time.Now().Add(writeWait))
 			if !ok {
@@ -200,7 +214,7 @@ func (p *player) writePump() {
 
 			w, err := p.conn.NextWriter(websocket.TextMessage)
 			if err != nil {
-				log.Println(err)
+				log.Println("Could not make next writer:", err)
 				return
 			}
 			w.Write(msgB)
@@ -222,13 +236,13 @@ func (p *player) writePump() {
 			}
 
 			if err := w.Close(); err != nil {
-				log.Println(err)
+				log.Println("Could not close writer:", err)
 				return
 			}
 		case <-ticker.C: // ping
 			p.conn.SetWriteDeadline(time.Now().Add(writeWait))
 			if err := p.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
-				log.Println(err)
+				log.Println("Could not ping:", err)
 				return
 			}
 		case <-p.clock.C: // Player ran out ouf time
@@ -239,7 +253,7 @@ func (p *player) writePump() {
 				"OOT": "MY_CLOCK",
 			}
 			if err := sendTextMsg(data, p.conn); err != nil {
-				log.Println(err)
+				log.Println("Could not send text msg:", err)
 				return
 			}
 		case <-p.oppRanOut: // Opponent ran out ouf time
@@ -247,7 +261,7 @@ func (p *player) writePump() {
 				"OOT": "OPP_CLOCK",
 			}
 			if err := sendTextMsg(data, p.conn); err != nil {
-				log.Println(err)
+				log.Println("Could not send text msg:", err)
 				return
 			}
 		case <-p.drawOffer: // Opponent offered draw
@@ -255,7 +269,7 @@ func (p *player) writePump() {
 				"drawOffer": "true",
 			}
 			if err := sendTextMsg(data, p.conn); err != nil {
-				log.Println(err)
+				log.Println("Could not send text msg:", err)
 				return
 			}
 		case <-p.oppAcceptedDraw: // opponent accepted draw
@@ -263,7 +277,7 @@ func (p *player) writePump() {
 				"oppAcceptedDraw": "true",
 			}
 			if err := sendTextMsg(data, p.conn); err != nil {
-				log.Println(err)
+				log.Println("Could not send text msg:", err)
 				return
 			}
 		case <-p.oppResigned: // opponent resigned
@@ -271,7 +285,7 @@ func (p *player) writePump() {
 				"oppResigned": "true",
 			}
 			if err := sendTextMsg(data, p.conn); err != nil {
-				log.Println(err)
+				log.Println("Could not send text msg:", err)
 				return
 			}
 		case <-p.rematchOffer: // Opponent offered rematch
@@ -279,7 +293,7 @@ func (p *player) writePump() {
 				"rematchOffer": "true",
 			}
 			if err := sendTextMsg(data, p.conn); err != nil {
-				log.Println(err)
+				log.Println("Could not send text msg:", err)
 				return
 			}
 		case <-p.oppAcceptedRematch: // opponent accepted rematch
@@ -287,7 +301,39 @@ func (p *player) writePump() {
 				"oppAcceptedRematch": "true",
 			}
 			if err := sendTextMsg(data, p.conn); err != nil {
-				log.Println(err)
+				log.Println("Could not send text msg:", err)
+				return
+			}
+		case <-p.oppReady: // opponent ready
+			data := map[string]string{
+				"oppReady": "true",
+			}
+			if err := sendTextMsg(data, p.conn); err != nil {
+				log.Println("Could not send text msg:", err)
+				return
+			}
+		case <-p.oppDisconnected: // opponent disconnected
+			data := map[string]string{
+				"waitingOpp": "true",
+			}
+			if err := sendTextMsg(data, p.conn); err != nil {
+				log.Println("Could not send text msg:", err)
+				return
+			}
+		case <-p.oppReconnected: // opponent reconnected
+			data := map[string]string{
+				"oppReady": "true",
+			}
+			if err := sendTextMsg(data, p.conn); err != nil {
+				log.Println("Could not send text msg:", err)
+				return
+			}
+		case <-p.oppGone: // opponent is gone
+			data := map[string]string{
+				"oppGone": "true",
+			}
+			if err := sendTextMsg(data, p.conn); err != nil {
+				log.Println("Could not send text msg:", err)
 				return
 			}
 		}
@@ -314,7 +360,8 @@ func sendTextMsg(data map[string]string, conn *websocket.Conn) error {
 
 // serveGame handles websocket requests from the peer.
 func (rout *router) serveGame(w http.ResponseWriter, r *http.Request,
-	gameId, color string, minutes int, cleanup func(), username, userId string) {
+	gameId, color string, minutes int, cleanup, switchColors func(),
+	username, userId string) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
@@ -329,14 +376,20 @@ func (rout *router) serveGame(w http.ResponseWriter, r *http.Request,
 		color:              color,
 		conn:               conn,
 		gameId:             gameId,
-		oppRanOut:          make(chan bool),
-		drawOffer:          make(chan bool),
-		oppAcceptedDraw:    make(chan bool),
-		oppResigned:        make(chan bool),
-		rematchOffer:       make(chan bool),
-		oppAcceptedRematch: make(chan bool),
+		oppRanOut:          make(chan bool, 1),
+		disconnect:         make(chan bool),
+		drawOffer:          make(chan bool, 1),
+		oppAcceptedDraw:    make(chan bool, 1),
+		oppResigned:        make(chan bool, 1),
+		rematchOffer:       make(chan bool, 1),
+		oppAcceptedRematch: make(chan bool, 1),
+		oppReady:           make(chan bool, 1),
+		oppDisconnected:    make(chan bool, 1),
+		oppGone:            make(chan bool, 1),
+		oppReconnected:     make(chan bool, 1),
 		sendMove:           make(chan []byte, 2), // one for the clock, one for the move
 		sendChat:           make(chan message, 128),
+		switchColors:       switchColors,
 		timeLeft:           time.Duration(minutes) * time.Minute,
 		userId:             userId,
 		username:           username,
@@ -361,5 +414,5 @@ func (rout *router) serveGame(w http.ResponseWriter, r *http.Request,
 	go p.writePump()
 	go p.readPump()
 
-	rout.ldHub.startGame<- true
+	rout.ldHub.joinPlayer<- userId
 }
